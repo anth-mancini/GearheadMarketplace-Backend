350f781c84d0917c994f78b14a407cc5fc2527cf
felixdapony2 <85595042+felixdapony2@users.noreply.github.com>
2021-07-01 18:33:41 -0400

    Update api.py

--- api.py
+++ api.py
@@ -54,16 +54,16 @@ async def return_server_status():
         return "online"
     return "offline"
 
-# @app.post("/users/", response_model=schemas.User)
-# def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-#     db_user = crud.get_user_by_email(db, email=user.email)
-#     if db_user:
-#         raise HTTPException(status_code=400, detail="Email already registered")
-#     return crud.create_user(db=db, user=user)
 @app.post("/users/", response_model=schemas.User)
-def create_user(jsonobject: jsonobject):
-    print(jsonobject)
-    return jsonobject
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
+# @app.post("/users/", response_model=schemas.User)
+# def create_user(jsonobject: jsonobject):
+#     print(jsonobject)
+#     return jsonobject
     # db_user = crud.get_user_by_email(db, email=user.email)
     # if db_user:
     #     raise HTTPException(status_code=400, detail="Email already registered")
